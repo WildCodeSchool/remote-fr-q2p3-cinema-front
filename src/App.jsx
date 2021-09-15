56214312078457d92510d3a0785c71c592a8d39d
anne-marziale <annemarziale@hotmail.com>
2021-09-15 09:46:53 +0200

    router created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css';
-import Slider from './components/slider/Slider';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import Footer from './components/footer/Footer';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       CINEMA FRONT
-      <Slider />
+      <Router>  
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+        </Router> 
       <Footer />
     </div>
   );
--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
+import React, { Component } from "react";
+import Slider from "react-slick";
 import image1 from '../../images/1.jpg';
 import image2 from '../../images/2.jpg';
 import image3 from '../../images/3.jpg';
@@ -10,71 +9,51 @@ import image6 from '../../images/6.jpg';
 import image7 from '../../images/7.jpg';
 import image8 from '../../images/8.jpg';
 import image9 from '../../images/9.jpg';
-import "./Slider.css"
+import './Slider.css';
 
-function Slider() {
-    const datas = [
-        {
-            id:1,
-            image: `${image1}`,
-            title: "Titre du slider1",
-        },
-        {
-            id:2,
-            image: `${image2}`,
-            title: "Titre du slider1",
-        }, {
-            id:3,
-            image: `${image3}`,
-            title: "Titre du slider1",
-        }, {
-            id:4,
-            image: `${image4}`,
-            title: "Titre du slider1",
-        }, {
-            id:5,
-            image: `${image5}`,
-            title: "Titre du slider1",
-        }, {
-            id:6,
-            image: `${image6}`,
-            title: "Titre du slider1",
-        }, {
-            id:7,
-            image: `${image7}`,
-            title: "Titre du slider1",
-        },{
-            id:8,
-            image: `${image8}`,
-            title: "Titre du slider1",
-        },{
-            id:9,
-            image: `${image9}`,
-            title: "Titre du slider1",
-        }
-    ]
+export default class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
     return (
-
-        <Carousel
-        autoPlay
-        interval={6000}
-        infiniteLoop
-        thumbWidth={120}
-        showIndicators={false}
-        showStatus={false}
-        
-        >
-           {datas.map ( slide => (
-               <div key={slide.id}>
-                   <img src={slide.image} alt="" />
-                   <div className="overlay">
-                       <h2 className="overlay__title">{slide.title}</h2>
-                       <p className="overlay__text"></p>
-                   </div>
-               </div>
-           ))}
-        </Carousel>
-    )
+      <div>
+        <h2> Carousel</h2>
+        <Slider {...settings}>
+          <div>
+            <h3>{image1}</h3>
+          </div>
+          <div>
+            <h3>{image2}</h3>
+          </div>
+          <div>
+            <h3>{image3}</h3>
+          </div>
+          <div>
+            <h3>{image4}</h3>
+          </div>
+          <div>
+            <h3>{image5}</h3>
+          </div>
+          <div>
+            <h3>{image6}</h3>
+          </div>
+          <div>
+            <h3>{image7}</h3>
+          </div>
+          <div>
+            <h3>{image8}</h3>
+          </div>
+          <div>
+            <h3>{image9}</h3>
+          </div>
+        </Slider>
+      </div>
+    );
+  }
 }
 
-export default Slider;
