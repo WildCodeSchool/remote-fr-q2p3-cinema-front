d03ea0a0efe63ecf6ac46d71d3efae330c4f8f93
Kameli71 <karim.kameli71@gmail.com>
2021-09-29 09:02:22 +0200

    link backend/frontend card movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import MovieCard from './components/MovieCard';
+import MovieList from './pages/MovieList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      CINEMA FRONT
+      <Router>    
+        <Switch>
+          <Route exact path="/movies" component={MovieList}/>
+        </Switch>
+        </Router> 
     </div>
   );
 }
