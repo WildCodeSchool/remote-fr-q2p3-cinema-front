039d3fb8784b49132323954bc457e855adaa44b2
BenSbnt <b.bonnet97@hotmail.fr>
2021-10-07 15:36:36 +0200

    filter test

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Router>  
+      <Router>
         <NavBar />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path ="/Supported-Films" component = {SupportedFilms} />
-          <Route path ="Funding" component = {Funding} />
-          <Route exact path="/movies" component={MovieList}/>
+          <Route path="/Supported-Films" component={SupportedFilms} />
+          <Route path="/Funding" component={Funding} />
+          <Route exact path="/movies" component={MovieList} />
         </Switch>
-       </Router> 
+      </Router>
       <Footer />
     </div>
   );
--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -4,25 +4,76 @@ import MovieCard from '../components/MovieCard';
 
 function MovieList() {
     const [movies, setMovies] = useState([]);
-    // const [moviesPlay, setMoviesPlay] = useState();
-    // const movieID = props.match.params.id;
+    const [movie, setMovie] = useState("");
 
     useEffect(() => {
         axios
-        .get('http://localhost:8000/api/movies/subventions')
-        .then((res) => res.data)
-        .then((data) => setMovies(data))
-    }, [])
+            .get('http://localhost:8000/api/movies/subventions')
+            .then((res) => res.data)
+            .then((data) => setMovies(data))
+    }, [movies, movie])
+    const uniqueGenre = [];
+    movies.map(item => {
+        const findItem = uniqueGenre.find(x => x.FILM_GENRE === item.FILM_GENRE);
+        if (!findItem)
+            uniqueGenre.push(item);
+    });
+    const uniqueDate = [];
+    movies.map(item => {
+        const findItem = uniqueDate.find(x => x.FILM_ANNEE === item.FILM_ANNEE);
+        if (!findItem)
+            uniqueDate.push(item);
+    });
+    const handleChangeGenre = event => {
+        setMovie(event.target.value);
+    };
+
+    const filterDropdown = movies.filter(function (result) {
+        return result.FILM_GENRE === movie;
+    });
+    const filterDropdowndate = movies.filter(function (result) {
+        return result.FILM_ANNEE === movie;
+    });
     return (
-        <div className="Movies">
-            {/* <div className="movies-container"> */}
-                {movies
-                .map((movie, index) => 
-                <div key={index}>
-                   <MovieCard movie={movie}/> 
-                </div>
-                )}
-            {/* </div> */}
+        <div className="Movies" >
+
+            <label>
+                Genre
+                <select value={movie} onChange={handleChangeGenre}>
+                    {uniqueGenre.map(movie => (
+                        <option>
+                            {movie.FILM_GENRE}
+                        </option>
+                    ))}
+                </select>
+            </label>
+            <label>
+                Année
+                <select value={movie} onChange={handleChangeGenre}>
+                    {uniqueDate.map(movie => (
+                        <option>
+                            {movie.FILM_ANNEE}
+                        </option>
+                    ))}
+                </select>
+            </label>
+            <div>
+                {filterDropdown.map(movie => (
+                    <div key={movie} style={{ margin: "10px" }}>
+                        <MovieCard movie={movie} />
+                        <br />
+                    </div>
+                ))}
+            </div>
+            <div>
+                {filterDropdowndate.map(movie => (
+                    <div key={movie} style={{ margin: "10px" }}>
+                        <MovieCard movie={movie} />
+                        <br />
+                    </div>
+                ))}
+            </div>
+
             {/* <div className="searchBar">
                 <input type="text" name="searchBar" id="searchBar" placeholder="Rechercher"/>
             </div>
