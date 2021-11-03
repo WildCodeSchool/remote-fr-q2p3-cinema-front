c620db2ac6169bb5c071472e5d3b49f64ae71f27
anne-marziale <annemarziale@hotmail.com>
2021-11-03 18:04:40 +0100

    css cards done

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import NavBar from './components/navbar/NavBar';
 import Shooting from './components/Shooting/Shooting';
 import Footer from './components/footer/Footer';
 import FundingList from './components/Funding/FundingList';
-import About from './components/About/About';
-import Modale from './components/movies/Modale';
 import './App.css';
 
 function App() {
@@ -16,11 +14,10 @@ function App() {
         <NavBar />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/shooting" component={Shooting} />
-          <Route path="/about" component={About} />
           <Route path="/funding" component={FundingList} />
+          <Route path="/shooting" component={Shooting} />
+          <Route path="/list" component={FundingList} />
           <Route exact path="/movies" component={MovieList} />
-          <Route exact path="/movies/:FILM_TITRE" component={Modale} />
         </Switch>
       </Router>
       <Footer />
--- src/components/Funding/FundingList.jsx
+++ src/components/Funding/FundingList.jsx
@@ -6,88 +6,114 @@ import Funding from './Funding';
 function FundingList() {
   const [fundings, setFundings] = useState([]);
   const [openModal, setOpenModal] = useState(null);
- 
+
   useEffect(() => {
     axios
       .get('http://localhost:8000/api/financements')
       .then((res) => res.data)
       .then((data) => setFundings(data));
   }, []);
- 
+
   const showModal = (id) => {
     setOpenModal(id);
   };
- 
+
   const hideModal = () => {
     setOpenModal(false);
   };
- 
+
   return (
     <div className="Logo">
       <Logo />
       <div className="Funding">
-          <div className="funding-text">
-            <div className="Funding-page"> Accueil Financer votre projet</div>
-            <h1 className="funding-main-title">
-              UN FONDS D'AIDE POUR LA CREATION ET LA PRODUCTION AUDIOVISUELLE ET
-              CINEMATOGRAPHIQUE
-            </h1>
-            <h4 className="funding-second-title">
-              En partenariat avec le CNC (Centre national du cinéma et de
-              l'image animée), la Région Sud aide à la création et à la
-              production d'oeuvres cinématographiques et audiovisuelles de
-              qualité dans toute leur diversité. Des aides à l'écriture, en
-              développement et en production sont proposées selon les types de
-              projet. Les aides à l'écriture, versées directement aux auteurs,
-              prennent la forme de bourses d'écriture.
-            </h4>
-          </div>
+        <div className="funding-text">
+          <div className="Funding-page"> Accueil Financer votre projet</div>
+          <h1 className="funding-main-title">
+            UN FONDS D'AIDE POUR LA CREATION ET LA PRODUCTION AUDIOVISUELLE ET
+            CINEMATOGRAPHIQUE
+          </h1>
+          <h4 className="funding-second-title">
+            En partenariat avec le CNC (Centre national du cinéma et de l'image
+            animée), la Région Sud aide à la création et à la production
+            d'oeuvres cinématographiques et audiovisuelles de qualité dans toute
+            leur diversité. Des aides à l'écriture, en développement et en
+            production sont proposées selon les types de projet. Les aides à
+            l'écriture, versées directement aux auteurs, prennent la forme de
+            bourses d'écriture.
+          </h4>
         </div>
- 
+      </div>
+
       <div className="FundingList">
         {fundings.map((funding) => (
           <div key={funding.id}>
-            <div className="funding-column" onClick={() => showModal(funding.id)}>{funding.FIN_FORMATS}</div>
+            <div
+              className="funding-column"
+              onClick={() => showModal(funding.id)}
+            >
+              {funding.FIN_FORMATS}
+            </div>
             <Funding
               showModal={openModal}
               hideModal={hideModal}
               funding={funding}
             >
-              <div className="modalBodyTitle">
-                {/* <h3>LONG MÉTRAGE</h3>
-                <div className="text-modal-funding">
-                  {' '}
-                  Ce fond répond à des modalités pratiques des aides à
-                  l'écriture, au développement et à la production des oeuvres
-                  cinématographiques de fiction de longue durée. Encadrées par
-                  la réglementation européenne, ces aides s'inscrivent dans la
-                  convetion de partenariat avec le Centre national du cinéma et
-                  de l'image animée (CNC) et son définies par le cadre
-                  d'intervention adopté par la Région.
-                </div> */}
-              </div>
               <div className="modalBodyFin">
                 <div className="wrapper">
-                <p>{funding.FIN_FORMATS}</p>
-                <p>{funding.FIN_DESCRIPTIF}</p>
-                <div className="contact-funding">
-                   
-                  <p>{funding.FIN_CONTACT}</p>
-                <p>{funding.FIN_MAIL}</p>
-                <p>{funding.FIN_TELEPHONE}</p>
+                  <div className="props-content">
+                    <h2 className="title-use-doc">DOCUMENTS UTILES</h2>
+                    {/* <p className="props-funding">{funding.FIN_FORMATS}</p> */}
+                    <p className="props-funding">{funding.FIN_STRUCTURE}</p>
+                    <h2 className="title-help-fund">DEMANDE D'AIDE FINANCIERE</h2>
+                    <p className="props-funding">{funding.FIN_DESCRIPTIF}</p>
+                      <p className="props-funding">{funding.FIN_CONTACT}</p>
+                      <p className="props-funding">{funding.FIN_TELEPHONE}</p>
+                      <p className="props-funding">{funding.FIN_MAIL}</p>
+                      <h2 className="title-contact">CONTACT</h2>
+                    <p className="props-funding funding-email"> Email :  {funding.FIN_WEB}</p>
+                    <p className="props-funding">{funding.FIN_COMMUNE_INSEE}</p>
+                    <p className="props-funding">{funding.FIN_COMMUNE}</p>
+                    <p className="props-funding">{funding.FIN_INTERCO}</p>
+                    <p className="props-funding">{funding.FIN_DEPT}</p>
+                  </div>
+                  <div className="second-modal-part">
+                    <div className="modalBodyTitle">
+                      <h3 className="h3-title">LONG MÉTRAGE</h3>
+                      <div className="text-modal-funding">
+                        Ce fond répond à des modalités pratiques des aides à
+                        l'écriture, au développement et à la production des
+                        oeuvres cinématographiques de fiction de longue durée.
+                        Encadrées par la réglementation européenne, ces aides
+                        s'inscrivent dans la convetion de partenariat avec le
+                        Centre national du cinéma et de l'image animée (CNC) et
+                        son définies par le cadre d'intervention adopté par la
+                        Région.
+                      </div>
+                    </div>
+                    <div className="second-text-modal">
+                      <p className="text-1">
+                        <p className="comity-title">1# Comité de lecture</p>
+                        <p>Date limite de dépôt des dossiers : 30 septembre 2019</p>
+                           Date du comité de lecture : 12 décembre 2020
+                      </p>
+                      <p className="text-2">
+                        <p className="comity-title">2# Comité de lecture</p>
+                        <p>Date limite de dépôt des dossiers : 30 janvier 2020</p>
+                        Date du comité de lecture : 25 mars 2020
+                      </p>
+                      <p className="text-3">
+                        <p className="comity-title">3# Comité de lecture</p>
+                        <p>Date limite de dépôt des dossiers : 30 avril 2020</p>
+                        Date du comité de lecture : 6 juillet 2020
+                      </p>
+
+                    </div>
+                  </div>
+                  <div className="faq-button">FAQ</div>
+                  <button className="close">x</button>
                 </div>
-                <p>{funding.FIN_WEB}</p>
-                <p>{funding.FIN_COMMUNE_INSEE}</p>
-                <p>{funding.FIN_COMMUNE}</p>
-                <p>{funding.FIN_INTERCO}</p>
-                <p>{funding.FIN_DEPT}</p>
-                <p>{funding.FIN_REGION}</p> 
-                <div className="faq-button">FAQ</div>
-                <button className="close">x</button>
               </div>
-              </div>
-              <div className="modalFooter">
-              </div>
+              <div className="modalFooter"></div>
             </Funding>
           </div>
         ))}
@@ -95,5 +121,5 @@ function FundingList() {
     </div>
   );
 }
- 
+
 export default FundingList;
